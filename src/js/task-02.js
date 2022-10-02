bfa46fe704ad3c3a70a3b2f42cdd3b42ecbaa626
Yev1999 <92596319+Yev1999@users.noreply.github.com>
2022-10-02 17:10:47 +0300

    work2

--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,21 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const addElenents = () => {
+  let newIngredients = [];
+  ingredients.map((ingredient) => {
+    const liItem = document.createElement("li");
+    liItem.innerText = `${ingredient}`;
+    liItem.classList.add("item");
+    newIngredients.push(liItem);
+  });
+  const arrayIngredients = document.querySelector("ul#ingredients");
+  arrayIngredients.append(...newIngredients);
+};
+addElenents();
