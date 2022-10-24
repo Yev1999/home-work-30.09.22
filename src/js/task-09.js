ffda6e9d6f025ea1e57dff510e9f5b7009c6ca8d
Yev1999 <92596319+Yev1999@users.noreply.github.com>
2022-10-24 13:14:36 +0300

    task-09

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,13 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+const bodyColor = document.querySelector("body");
+const changeColor = document.querySelector(".change-color");
+const color = document.querySelector(".color");
+
+function changeBodyColor() {
+  bodyColor.style.backgroundColor = getRandomHexColor();
+  color.textContent = bodyColor.style.backgroundColor;
+}
+
+changeColor.addEventListener("click", changeBodyColor);
